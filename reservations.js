ae4ff1d9292c8c8ddbdfc9b0e9138b013686975e
Andrew Orr <andreworr@Andrews-MacBook-Pro.local>
2017-11-09 12:00:01 -0600

    completed

---
+++ reservations.js
@@ -0,0 +1,36 @@
+// var Reservation = function(name, number, email, id){
+//     this.name = name;
+//     this.number = number;
+//     this.email = email;
+//     this.id = id;
+//   };
+
+  var reservationList = [
+  {
+  "name": "John Smith1",
+  "number": "5129768989",
+  "email": "jsmith@aol.com",
+  "iD": "jj101"
+  },
+  {
+  "name": "John Smith2",
+  "number": "5129768989",
+  "email": "jsmith@aol.com",
+  "iD": "jj101"
+  },
+  {
+  "name": "John Smith3",
+  "number": "5129768989",
+  "email": "jsmith@aol.com",
+  "iD": "jj101"
+  },
+  {
+  "name": "John Smith4",
+  "number": "5129768989",
+  "email": "jsmith@aol.com",
+  "iD": "jj101"
+  },
+
+];
+
+module.exports = reservationList;
